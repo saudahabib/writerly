e651a15dfdc9e64a372b4f75222dcfc2d5285e45
saudahabib <saudababs00@gmail.com>
2019-04-27 17:19:17 +0300

    create models

---
+++ flaskblog/config.py
@@ -0,0 +1,31 @@
+import os
+
+class Config:
+    SECRET_KEY = 'b21f90fdcd2db419e7883c49949f58fb'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME='saudababs00@gmail.com'
+    MAIL_PASSWORD='Her4631!'
+
+
+
+class DevConfig(Config):
+    '''
+    Class that holds configurations in development mode
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://student:1234@localhost/writerly'
+    debug = True
+
+class ProdConfig(Config):
+    '''
+    '''
+
+
+    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ manage.py
@@ -0,0 +1,16 @@
+from flaskblog import create_app, db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('production')
+manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db)
+
+if __name__ == '__main__':
+    manager.run()
